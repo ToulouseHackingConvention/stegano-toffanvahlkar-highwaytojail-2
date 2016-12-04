5f3a99e69d4299da5f8e0768755344faa05c349f
toffan <benoit.lemarchand@inpt.fr>
2016-12-04 10:48:33 +0100

    Add first sources

---
+++ solution/solve.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import sys
+from PIL import Image
+
+filename = sys.argv[1] if len(sys.argv) == 2 else "wallpaper.png"
+img = Image.open(filename).convert('RGB')
+
+MASK = 0x3
+X, Y = img.size[0], 20
+
+secret = []
+for y in range(Y):
+    for x in range(0, X, 3):
+        r, _, _ = img.getpixel((x, y))
+        secret.append(format(r & MASK, '02b'))
+        # secret is a list of 2 bits strings. Ex: ['01', '11', '01', ...]
+
+
+secret = ["".join(secret[i:i + 4]) for i in range(0, len(secret), 4)]
+secret = "".join(chr(int(s, 2)) for s in secret)
+
+print(secret)
---
+++ src/insert.cpp
@@ -0,0 +1,96 @@
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+#include <bitset>
+
+#include <SFML/Graphics/Image.hpp>
+
+
+const std::uint8_t MASK{0x3};
+const std::uint8_t MASKSIZE{2};
+
+void usage(int argc, char** argv);
+std::vector<std::uint8_t> split(const std::string& filename);
+std::vector<std::uint8_t> split(std::ifstream& file);
+
+
+int main(int argc, char** argv) {
+    usage(argc, argv);
+    std::string src_filename = argv[1];
+    std::string secret_filename = argv[2];
+    std::string dst_filename = argv[3];
+
+    sf::Image img;
+    if (!img.loadFromFile(src_filename)) {
+        std::cerr << "Error: cannot open file " << argv[1] << ". Exiting..."
+                  << std::endl;
+        exit(1);
+    }
+
+    std::size_t x = 0, y = 0;
+    const std::size_t X = img.getSize().x, Y = img.getSize().y;
+    const std::size_t i = 3, j = 1;
+
+    for (std::uint8_t d : split(secret_filename)) {
+        // Hide data
+        auto pix = img.getPixel(x, y);
+        std::uint8_t r = (pix.r & ~MASK) | d;
+        img.setPixel(x, y, {r, pix.g, pix.b});
+
+        // Step
+        x += i;
+        if (x >= X) {
+            x %= X;
+            y += j;
+            if (y >= Y) {
+                std::cerr << "Error: image is not large enough." << std::endl;
+                return 1;
+            }
+        }
+    }
+
+    img.saveToFile(dst_filename);
+    return 0;
+}
+
+
+void usage(int argc, char** argv) {
+    if (argc != 4) {
+        std::cout << "Usage: " << argv[0]
+                  << " PICTURE SECRET OUTPUT\n"
+                     "   PICTURE     filename of the picture to modify\n"
+                     "   SECRET      filename of the secret to hide\n"
+                     "   OUTPUT      filename of the output"
+                  << std::endl;
+        exit(1);
+    }
+}
+
+
+std::vector<std::uint8_t> split(const std::string& filename) {
+    std::ifstream file(filename, std::ios::binary);
+    if (!file) {
+        std::cerr << "Error: cannot open file " << filename << ". Exiting..."
+                  << std::endl;
+        exit(1);
+    } else {
+        return split(file);
+    }
+}
+
+
+std::vector<std::uint8_t> split(std::ifstream& file) {
+    std::vector<uint8_t> data;
+
+    while (!file.eof()) {
+        char c = file.get();
+        data.push_back((c >> 6) & MASK);
+        data.push_back((c >> 4) & MASK);
+        data.push_back((c >> 2) & MASK);
+        data.push_back(c & MASK);
+    }
+
+    return data;
+}
+
